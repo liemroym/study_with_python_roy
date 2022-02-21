1d4d438682276a6c1fd0f8dedc7adc8f2d2573f6
liemroym <liemroymarcelino@gmail.com>
2022-02-21 18:48:10 +0700

    Made each branches represent one folder

--- QR/sudoku.py
+++
@@ -1,8 +0,0 @@
-field = [['.' for row in range(9)] for col in range(9)]
-
-def InitField():
-    for row in field:
-        for i in range(0, 9, 3):
-            print('j'.join(str(row)))
-
-InitField()
