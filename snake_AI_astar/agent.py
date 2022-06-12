a6c06f870ca81ee806d3f3a8f79cc80f6b3d79bb
liemroym <liemroymarcelino@gmail.com>
2022-06-12 22:20:00 +0700

    Only find path each time food appears (instead of each time snake moves)

--- snake_AI_astar/agent.py
+++ snake_AI_astar/agent.py
@@ -7,7 +7,7 @@ class Agent:
     def __init__(self):
         self.game : Game = Game()
 
-    def find_path(self, state) -> str: 
+    def find_path(self, state): 
         snake = state[1]
         food = state[2]
 
@@ -23,9 +23,9 @@ class Agent:
             for dir in around:
                 curr_X = node[1][0] + dir[0]
                 curr_Y = node[1][1] + dir[1]
-                if (curr_X > 0 and curr_X < SCREEN_SIZE[0] // GRID_SIZE):
-                    if (curr_Y > 0 and curr_Y < SCREEN_SIZE[1] // GRID_SIZE):
-                        if (not ((curr_X, curr_Y) in snake or (curr_X, curr_Y) in list(n[1] for n in history) or (curr_X, curr_Y) in list(n[1] for n in to_visit))):
+                if (curr_X >= 0 and curr_X < SCREEN_SIZE[0] // GRID_SIZE):
+                    if (curr_Y >= 0 and curr_Y < SCREEN_SIZE[1] // GRID_SIZE):
+                        if (not((curr_X, curr_Y) in snake or (curr_X, curr_Y) in list(n[1] for n in history) or (curr_X, curr_Y) in list(n[1] for n in to_visit))):
                             cost = abs(curr_X - food[0]) + abs(curr_Y - food[1]) + node[3]
                             to_visit.append((cost, (curr_X, curr_Y), node[1], node[3]+1))
                 
@@ -35,27 +35,36 @@ class Agent:
             to_visit.sort(key=lambda t: t[0])
 
         # Find direction
+        move = []
         curr_node = history[-1]
         for node in reversed(history):
-            if (node != history[0]):
-                if (curr_node[2] == node[1]):
-                    curr_node = node
+            if (curr_node[2] == node[1]):
+                if (curr_node[2][0]-curr_node[1][0] == -1):
+                    move.append("RIGHT")
+                elif (curr_node[2][0]-curr_node[1][0] == 1):
+                    move.append("LEFT")
+                elif (curr_node[2][1]-curr_node[1][1] == -1):
+                    move.append("DOWN")
+                elif (curr_node[2][1]-curr_node[1][1] == 1):
+                    move.append("UP")
+                curr_node = node
 
-        if (curr_node[2][0]-curr_node[1][0] == -1):
-            return "RIGHT"
-        elif (curr_node[2][0]-curr_node[1][0] == 1):
-            return "LEFT"
-        elif (curr_node[2][1]-curr_node[1][1] == -1):
-            return "DOWN"
-        elif (curr_node[2][1]-curr_node[1][1] == 1):
-            return "UP"
+        return list(reversed(move))
 
     def main_loop(self):
-        state = self.game.get_state()
-        print(state)
-        self.game.direction = self.find_path(state)
+        while True:
+            state = self.game.get_state()
+            # print(state)
+            moves = self.find_path(state)
+            # print(self.game.direction, moves)
+            if (moves != []):
+                for move in moves:
+                    self.game.direction = move
+                    self.game.startGame()
+            else:
+                self.game.startGame()
+            
         # print(self.game.direction)
-        self.game.startGame()
 
 if __name__ == "__main__":
     agent = Agent()
@@ -63,7 +72,5 @@ if __name__ == "__main__":
     # Test python stuff
     # history = [(0, (1, 1), (2, 2), 1), (10, (4, 2), (3, 1), 2), (5, (10, 7), (4, 5), 10)]
     # print((1, 2) in list(n[1] for n in history))
-
-    while True:
-        agent.main_loop()
+    agent.main_loop()
         
